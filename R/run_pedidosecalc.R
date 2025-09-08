54038c278c1fccde7b61e909137bdf0421f21f42
melissameekerphd <mmeeker@mgh.harvard.edu>
2025-09-08 11:06:39 -0400

    Working to make system.file function as expected.

--- R/run_pedidosecalc.R
+++ R/run_pedidosecalc.R
@@ -3,5 +3,5 @@
 #' @export
 run_app <- function(...) {
   app_dir <- system.file("shinypedidosecalc", package = "pedidosecalc")
-  shiny::runApp(app_dir, display.mode = "normal", ...)
+  shiny::runApp(app_dir, display.mode = "normal")
 }
